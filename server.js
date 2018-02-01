e445d9da062492bb5a91d5cc64d7f72b4e903235
radeshshiwlochan <radeshshiwlochan>
2018-02-01 16:44:46 -0500

    adds navbar to the pages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express    = require( 'express' );
 const bodyParser = require( 'body-parser' );
+const path       = require( 'path' ); 
 const hbs        = require( 'hbs' );
 
 let port   = process.env.PORT || 3000;
